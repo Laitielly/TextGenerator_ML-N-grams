910d2e76dfa18f209a3f04a15b5576e936e18ff5
Laitielly <78595002+Laitielly@users.noreply.github.com>
2022-09-05 14:55:15 +0300

    add seed

--- generate.py
+++ generate.py
@@ -7,11 +7,12 @@ def main():
     pars.add_argument('--model', dest='model', help='Input the directory of model')
     pars.add_argument('--prefix', dest='prefix', help='Input start of the sentence')
     pars.add_argument('--length', dest='length', help='Input the length of the sentence')
+    pars.add_argument('--seed', dest='seed', help='Input the length of the sentence')
 
     args = pars.parse_args()
     generated = model.TextGenerator()
 
-    print(*generated.generate(args.model, args.prefix, int(args.length)))
+    print(*generated.generate(args.model, args.prefix, int(args.length), args.seed))
 
 
 main()
